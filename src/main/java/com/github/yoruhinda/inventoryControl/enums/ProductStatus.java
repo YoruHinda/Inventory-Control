c59e6fd7174615c0f1842d84009e55883afee101
yoruhinda <ruanphablo778@gmail.com>
2024-05-12 20:45:14 -0300

    Feat: Create Product Columns

---
+++ src/main/java/com/github/yoruhinda/inventoryControl/enums/ProductStatus.java
@@ -0,0 +1,5 @@
+package com.github.yoruhinda.inventoryControl.enums;
+
+public enum ProductStatus {
+    OUT_OF_STOCK, IN_STOCK;
+}
--- src/main/java/com/github/yoruhinda/inventoryControl/models/Product.java
+++ src/main/java/com/github/yoruhinda/inventoryControl/models/Product.java
@@ -1,4 +1,11 @@
 package com.github.yoruhinda.inventoryControl.models;
 
+import com.github.yoruhinda.inventoryControl.enums.ProductStatus;
+
 public class Product {
+    private Long productID;
+    private String name;
+    private Long quantity;
+    private float price;
+    private ProductStatus productStatus;
 }
